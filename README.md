ab0bb5a0c93744d21816c32b25a683e2211f4171
surajmt8848 <suraj8848mt@gmail.com>
2021-07-12 15:08:39 +0545

    health check handler

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # movies_api
+
 Movies API with GO
+
+<!-- For production -->
+
+go run ./cmd/api -port=3030 -env=production
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -20,7 +20,7 @@ type config struct {
 
 type application struct {
 	config config
-	loggrt *log.Logger
+	logger *log.Logger
 }
 
 func main() {
@@ -30,7 +30,7 @@ func main() {
 	// Read the value of the port and env command-line flags into the config struct. We
 	// default to using the port number 4000 and the environment "development" if no
 	// corresponding flags are provided.
-	flag.IntVar(&cfg.port, "port", 4000, "api server port")
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "environment(development|staging|production)")
 	flag.Parse()
 
@@ -48,17 +48,17 @@ func main() {
 	// Declare a new servemux and add a /v1/healthcheck route which dispatches requests
 	// to the healthcheckHandler method (which we will create in a moment).
 	mux := http.NewServeMux()
-	mux.HandleFunc("v1/healthcheck", app.healthcheckHandler)
+	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
 
 	// Declare a HTTP server with some sensible timeout settings, which listens on the
 	// port provided in the config struct and uses the servemux we created above as the
 	// handler.
 	srv := &http.Server{
-		Addr:         fmt.Sprintf("%d", cfg.port),
+		Addr:         fmt.Sprintf(":%d", cfg.port),
 		Handler:      mux,
 		IdleTimeout:  time.Minute,
-		ReadTimeout:  time.Second,
-		WriteTimeout: time.Second,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
 	}
 
 	// Start the HTTP server.
