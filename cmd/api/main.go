e9753f5727217316bde9125a22d40dc0c72980f0
surajmt8848 <suraj8848mt@gmail.com>
2021-07-12 14:17:22 +0545

    create http server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,69 @@
 package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+// Declaring version for our app
+const version = "1.0.0"
+
+// Define a config struct to hold all the configuration settings for our application.
+type config struct {
+	port int
+	env  string
+}
+
+type application struct {
+	config config
+	loggrt *log.Logger
+}
+
+func main() {
+	// Declare an instance of the config struct
+	var cfg config
+
+	// Read the value of the port and env command-line flags into the config struct. We
+	// default to using the port number 4000 and the environment "development" if no
+	// corresponding flags are provided.
+	flag.IntVar(&cfg.port, "port", 4000, "api server port")
+	flag.StringVar(&cfg.env, "env", "development", "environment(development|staging|production)")
+	flag.Parse()
+
+	// Initialize a new logger which writes messages to the standard out stream,
+	// prefixed with the current date and time.
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	// Declare an instance of the application struct, containing the config struct and
+	// the logger
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	// Declare a new servemux and add a /v1/healthcheck route which dispatches requests
+	// to the healthcheckHandler method (which we will create in a moment).
+	mux := http.NewServeMux()
+	mux.HandleFunc("v1/healthcheck", app.healthcheckHandler)
+
+	// Declare a HTTP server with some sensible timeout settings, which listens on the
+	// port provided in the config struct and uses the servemux we created above as the
+	// handler.
+	srv := &http.Server{
+		Addr:         fmt.Sprintf("%d", cfg.port),
+		Handler:      mux,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  time.Second,
+		WriteTimeout: time.Second,
+	}
+
+	// Start the HTTP server.
+	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
+	err := srv.ListenAndServe()
+	logger.Fatal(err)
+
+}
