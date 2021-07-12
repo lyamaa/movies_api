af86ca503310b33931d57e08743db5a2c53783a5
surajmt8848 <suraj8848mt@gmail.com>
2021-07-12 16:26:41 +0545

    formatting and enveloping responses

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -31,12 +31,13 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 	movie := data.Movie{
 		ID:        id,
 		CreatedAt: time.Now(),
+		Title:     "Rambo",
 		Runtime:   102,
 		Genres:    []string{"drama", "romance", "horror", "war"},
 		Version:   1,
 	}
 
-	err = app.writeJSON(w, http.StatusOK, movie, nil)
+	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
 	if err != nil {
 		app.logger.Println(err)
 		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
