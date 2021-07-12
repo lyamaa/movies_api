395ce6be7261f8646489782294198ccc1bc43477
surajmt8848 <suraj8848mt@gmail.com>
2021-07-12 17:40:58 +0545

    Triaging the Decode error

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
@@ -28,7 +27,7 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 	// struct as the target decode destination. If there was an error during decoding,
 	// we also use our generic errorResponse() helper to send the client a 400 Bad
 	// Request response containing the error message.
-	err := json.NewDecoder(r.Body).Decode(&input)
+	err := app.readJSON(w, r, &input)
 	if err != nil {
 		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
 		return
