bfa6cfb1695cdaa8310a44a42c3f3d0d953c3bbd
surajmt8848 <suraj8848mt@gmail.com>
2021-07-12 16:35:31 +0545

    Customizing the Runtime field

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -4,10 +4,10 @@ import "time"
 
 type Movie struct {
 	ID        int64     `json:"id"`
-	CreatedAt time.Time `json:"created_at"`
+	CreatedAt time.Time `json:"-"`
 	Title     string    `json:"title"`
-	Year      int32     `json:"year"`
-	Runtime   int32     `json:"runtime"`
-	Genres    []string  `json:"genres"`
+	Year      int32     `json:"year,omitempty"`
+	Runtime   Runtime   `json:"runtime,omitempty"`
+	Genres    []string  `json:"genres,omitempty"`
 	Version   int32     `json:"version"`
 }
