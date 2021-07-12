29671e94a7f2aa1ae119fc6bf9d2ae0662b63f7f
surajmt8848 <suraj8848mt@gmail.com>
2021-07-12 17:47:27 +0545

    create bad request error

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -29,7 +29,7 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 	// Request response containing the error message.
 	err := app.readJSON(w, r, &input)
 	if err != nil {
-		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		app.badRequestResponse(w, r, err)
 		return
 	}
 	// Dump the contents of the input struct in a HTTP response.
